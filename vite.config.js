ec4fa333887e3a0a3d6070c334bd10dacb59dd87
unknown <reinaldojunior272@gmail.com>
2023-10-28 22:57:54 -0300

    fd

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  publicPath: 'http://18.233.12.172/barbearia/',
-  base: 'http://18.233.12.172/barbearia/',
+  base: '/barbearia/',
+  index: path.resolve(__dirname, '../dist/index.html'),
+
+  // Paths
+  assetsRoot: path.resolve(__dirname, '../dist'),
+  assetsSubDirectory: 'static',
+  assetsPublicPath: '/',
   plugins: [vue()],
 
 
