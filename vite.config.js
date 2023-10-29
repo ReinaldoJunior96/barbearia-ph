13050c343bd0744d691d6b5a4ba31b8a21966c30
unknown <reinaldojunior272@gmail.com>
2023-10-28 22:52:52 -0300

    f

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  publicPath: '/barbearia/',
-  base: '/barbearia/',
+  publicPath: 'http://18.233.12.172/barbearia/',
+  base: 'http://18.233.12.172/barbearia/',
   plugins: [vue()],
 
 
